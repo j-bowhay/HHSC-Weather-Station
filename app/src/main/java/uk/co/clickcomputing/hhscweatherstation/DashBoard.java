39fc05e6a9c75666e2f643ed29606584bc148488
Jake <1clickcomputing@gmail.com>
2015-01-17 15:08:42 +0000

    Add the two fragments needed

---
+++ app/src/main/java/uk/co/clickcomputing/hhscweatherstation/DashBoard.java
@@ -0,0 +1,16 @@
+package uk.co.clickcomputing.hhscweatherstation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashBoard extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dashboard, container, false);
+    }
+}
