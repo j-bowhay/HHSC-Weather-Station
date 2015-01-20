ed119985cbd538f5cbb997ce02d28455d5d68abe
Jake <1clickcomputing@gmail.com>
2015-01-20 18:01:06 +0000

    Added swipe to refresh

--- app/src/main/java/uk/co/clickcomputing/hhscweatherstation/DashBoard.java
+++ app/src/main/java/uk/co/clickcomputing/hhscweatherstation/DashBoard.java
@@ -1,16 +1,34 @@
 package uk.co.clickcomputing.hhscweatherstation;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class DashBoard extends Fragment{
+public class DashBoard extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dashboard, container, false);
+        View layout = inflater.inflate(R.layout.dashboard, container, false);
+
+        SwipeRefreshLayout mSwipeRefreshLayout = (SwipeRefreshLayout) layout.findViewById(R.id.swipe);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        return layout;
+    }
+
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getActivity(), "Refreshing", Toast.LENGTH_SHORT).show();
+            }
+        }, 5000);
     }
 }
